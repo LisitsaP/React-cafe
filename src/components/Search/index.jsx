4644b6f71e1a0605b2af8f32fc10e449a538a4e3
LisitsaP <lisitsa.p.e@gmail.com>
2023-01-16 22:20:40 -0500

    changed queryselector to useRef in Search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,18 +2,21 @@ import React, { useContext } from "react";
 import { BiSearch, BiX } from "react-icons/bi";
 import styles from "./Search.module.scss";
 import { SearchContext } from "../../App";
+import { useRef } from "react";
 
 export default function Search() {
   const { searchValue, setSearchValue } = useContext(SearchContext);
+  const inputRef = useRef();
 
   const onClickClear = () => {
     setSearchValue("");
-    document.querySelector("input").focus();
+    inputRef.current.focus();
   };
   return (
     <div className={styles.root}>
       <BiSearch className={styles.icon} />
       <input
+        ref={inputRef}
         value={searchValue}
         placeholder="поиск"
         onChange={(event) => setSearchValue(event.target.value)}
